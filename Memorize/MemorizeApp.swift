21ea53c26275e3bd0452f9c47555d5516e4a68fc
Julio Rosario <juliorosario@Julios-MacBook-Pro.local>
2025-02-06 17:34:36 -0500

    Initial Commit: CardView & isFaceUp

---
+++ Memorize/MemorizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemorizeApp.swift
+//  Memorize
+//
+//  Created by Julio Rosario on 2/5/25.
+//
+
+import SwiftUI
+
+@main
+struct MemorizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
